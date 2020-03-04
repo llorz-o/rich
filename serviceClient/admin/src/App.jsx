059956faaa2824a8388f9b2386d8c16c92cb32b1
jojo-f <jojo@dcpt.online>
2020-03-04 18:12:08 +0800

    cls

---
+++ serviceClient/admin/src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import './App.css'
+
+import { ConfigProvider } from 'antd'
+
+function App() {
+  return (
+    <ConfigProvider>
+      <div className="App"></div>
+    </ConfigProvider>
+  )
+}
+
+export default App
